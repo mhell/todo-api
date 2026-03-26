4600526d51dd2edb8620ad87c90df5be74c3a6f2
Mattias Hellman <mattias.hellman@gmail.com>
2026-03-26 15:07:06 +0100

    Add emailService support

--- src/main/java/se/lexicon/todo_app/config/AppConfig.java
+++ src/main/java/se/lexicon/todo_app/config/AppConfig.java
@@ -2,10 +2,12 @@ package se.lexicon.todo_app.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import se.lexicon.notify.config.NotifyUtilConfig;
 
 @Configuration
 @ComponentScan("se.lexicon.todo_app.*")
 // To use NotifyUtil in your project, uncomment the line below to import the configuration from notify-util-spring module
-//@Import(NotifyUtilConfig.class)
+@Import(NotifyUtilConfig.class)
 public class AppConfig {
 }
--- src/main/java/se/lexicon/todo_app/service/PersonServiceImpl.java
+++ src/main/java/se/lexicon/todo_app/service/PersonServiceImpl.java
@@ -3,6 +3,8 @@ package se.lexicon.todo_app.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import se.lexicon.notify.model.Email;
+import se.lexicon.notify.service.MessageService;
 import se.lexicon.todo_app.dto.PersonDto;
 import se.lexicon.todo_app.dto.PersonRegistrationDto;
 import se.lexicon.todo_app.entity.Person;
@@ -19,16 +21,18 @@ import java.util.stream.Collectors;
 public class PersonServiceImpl implements PersonService {
     private final PersonRepository personRepository;
 
-    // To use notify-util-spring module for email service inject its dependency (MessageService<Email> emailService)
+    // To use notify-util-spring module for email service inject its dependency (
+    MessageService<Email> emailService;
 
     private final UserRepository userRepository;
 
     private PasswordEncoder passwordEncoder;
 
-    public PersonServiceImpl(PersonRepository personRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public PersonServiceImpl(PersonRepository personRepository, UserRepository userRepository, PasswordEncoder passwordEncoder, MessageService<Email> emailService) {
         this.personRepository = personRepository;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.emailService = emailService;
     }
 
 
@@ -71,7 +75,7 @@ public class PersonServiceImpl implements PersonService {
         // Save Person (cascade will save User)
         person = personRepository.save(person);
 
-        // emailService.sendMessage(new Email(person.getEmail(), "Welcome to Todo App", "Hello " + person.getName() + ",\n\nThank you for registering with us!"));
+        emailService.sendMessage(new Email(person.getEmail(), "Welcome to Todo App", "Hello " + person.getName() + ",\n\nThank you for registering with us!"));
 
         // Return DTO
         return convertToDto(person);
